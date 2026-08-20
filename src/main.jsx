1829634593607157dd364664558699f3cb11c478
Keshav <thakurkeshav112@gmail.com>
2026-08-20 15:53:25 +0530

    Center align hero title, search input card, START ANYWHERE pills and COMING SOON ON store badges

--- src/main.jsx
+++ src/main.jsx
@@ -3261,27 +3261,50 @@ function App() {
 
 
 
-          {/* App Store & Play Store Coming Soon Badges Row */}
+          {/* START ANYWHERE Topic Pills Row (Matching User Mockup) */}
           <motion.div 
-            className="hero-store-pills-row"
+            className="hero-start-anywhere-row"
+            initial={{ opacity: 0, y: 15 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.25 }}
+          >
+            <span className="start-anywhere-kicker">START ANYWHERE</span>
+            <div className="start-anywhere-pills">
+              {['A decision', 'Someone close', 'Work', 'Money', 'Myself'].map((pill, idx) => (
+                <button 
+                  key={idx} 
+                  type="button" 
+                  className="start-pill"
+                  onClick={() => setHeroInputText(`I want to reflect on ${pill.toLowerCase()}...`)}
+                >
+                  {pill}
+                </button>
+              ))}
+            </div>
+          </motion.div>
+
+          {/* COMING SOON ON App Store & Play Store Badges Row */}
+          <motion.div 
+            className="hero-coming-soon-row"
             initial={{ opacity: 0, y: 15 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.3 }}
           >
-            <div className="store-badge-pill">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 6.32c.67-.82 1.12-1.95.99-3.09-1 .04-2.17.67-2.87 1.49-.6.7-1.12 1.83-.98 2.94 1.12.09 2.21-.55 2.86-1.34z"/>
-              </svg>
-              <span>App Store</span>
-              <span className="badge-coming-soon-tag">Coming Soon</span>
-            </div>
+            <span className="coming-soon-kicker">COMING SOON ON</span>
+            <div className="coming-soon-pills">
+              <div className="store-badge-pill">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 6.32c.67-.82 1.12-1.95.99-3.09-1 .04-2.17.67-2.87 1.49-.6.7-1.12 1.83-.98 2.94 1.12.09 2.21-.55 2.86-1.34z"/>
+                </svg>
+                <span>App Store</span>
+              </div>
 
-            <div className="store-badge-pill">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M3.6 2.23A1.85 1.85 0 0 0 3 3.56v16.88a1.85 1.85 0 0 0 .6 1.33l.07.07 9.45-9.45v-.78L3.67 2.16l-.07.07zM16.7 15.17l-3.58-3.58v-.18l3.58-3.58 4.05 2.3c1.16.66 1.16 1.74 0 2.4l-4.05 2.64zM13.12 11.59l-9.45 9.45c.37.4 1 .45 1.54.14l10.9-6.23-2.99-3.36zM3.67 2.84l9.45 9.45 2.99-3.36L5.21 2.7c-.54-.31-1.17-.26-1.54.14z"/>
-              </svg>
-              <span>Google Play</span>
-              <span className="badge-coming-soon-tag">Coming Soon</span>
+              <div className="store-badge-pill">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M3.6 2.23A1.85 1.85 0 0 0 3 3.56v16.88a1.85 1.85 0 0 0 .6 1.33l.07.07 9.45-9.45v-.78L3.67 2.16l-.07.07zM16.7 15.17l-3.58-3.58v-.18l3.58-3.58 4.05 2.3c1.16.66 1.16 1.74 0 2.4l-4.05 2.64zM13.12 11.59l-9.45 9.45c.37.4 1 .45 1.54.14l10.9-6.23-2.99-3.36zM3.67 2.84l9.45 9.45 2.99-3.36L5.21 2.7c-.54-.31-1.17-.26-1.54.14z"/>
+                </svg>
+                <span>Google Play</span>
+              </div>
             </div>
           </motion.div>
 
